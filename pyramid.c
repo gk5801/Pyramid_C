63711a83d9ee0fd7ece8d0ac85ae2dfd56719283
gk5801 <gkawatra.cse18@chitkarauniversity.edu.in>
2021-01-05 21:43:49 +0530

    Create pyramid.c

---
+++ pyramid.c
@@ -0,0 +1,16 @@
+int main()
+{
+   int i,j,n;
+   scanf("%d",&n);
+   for(i=0;i<=n;i++)
+   {
+     for(j=1;j<=i;j++)
+       printf("%d",j);
+ 
+     for(j=i-1;j>=1;j--)
+	   printf("%d",j);
+     
+     printf("\n");
+   }
+  return 0;
+}
